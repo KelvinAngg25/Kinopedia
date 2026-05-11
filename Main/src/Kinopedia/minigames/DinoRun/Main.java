323509a3cc79f3c6352d9bbc41d6c85977302254
violafathiyah <violafathiyah14@gmail.com>
2026-05-11 15:00:45 +0700

    BIkin tampilan dinorun tetapi logic dan assets belum

---
+++ Main/src/Kinopedia/minigames/DinoRun/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kinopedia.minigames.DinoRun;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GameFrame frame = new GameFrame();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
