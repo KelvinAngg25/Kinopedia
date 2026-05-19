312368017e1b6cacbcff09841307ebef43f48d1c
Vincent <Vincent@LAPTOP-022IPGM4>
2026-05-19 11:03:43 +0700

    sudah ada 10 template tabel sudoku dan sudha bisa di random dan sudah ada alert kalau kredit 0

--- Main/src/Kinopedia/minigames/sudoku/Logic.java
+++ Main/src/Kinopedia/minigames/sudoku/Logic.java
@@ -26,6 +26,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 public class Logic extends JFrame implements Runnable{
     String p3;
     private int menit,detik;
@@ -85,44 +86,283 @@ public class Logic extends JFrame implements Runnable{
     
     private int[][] Puzzle1() {
         int[][] puzzle = new int[9][9];
-        puzzle[0] = new int[]{5, 2, 3, 1, 9, 4, 6, 8, 7};
-        puzzle[1] = new int[]{6, 8, 7, 3, 4, 2, 1, 5, 9};
-        puzzle[2] = new int[]{1, 9, 4, 6, 7, 5, 3, 2, 8};
-        puzzle[3] = new int[]{4, 6, 2, 5, 1, 9, 8, 3, 7};
-        puzzle[4] = new int[]{3, 1, 8, 4, 2, 7, 5, 9, 6};
-        puzzle[5] = new int[]{2, 7, 9, 8, 6, 3, 4, 5, 1};
-        puzzle[6] = new int[]{9, 5, 6, 7, 3, 1, 7, 4, 2};
-        puzzle[7] = new int[]{7, 4, 5, 2, 8, 6, 9, 1, 3};
-        puzzle[8] = new int[]{8, 3, 1, 0, 0, 0, 6, 7, 0};
+        puzzle[0] = new int[]{9, 0, 6, 0, 1, 0, 0, 7, 0};
+        puzzle[1] = new int[]{0, 5, 0, 7, 0, 3, 4, 0, 9};
+        puzzle[2] = new int[]{3, 0, 0, 9, 0, 8, 0, 6, 0};
+        puzzle[3] = new int[]{0, 1, 0, 0, 7, 2, 0, 8, 0};
+        puzzle[4] = new int[]{6, 0, 4, 0, 3, 0, 2, 0, 7};
+        puzzle[5] = new int[]{0, 7, 0, 5, 0, 9, 0, 1, 0};
+        puzzle[6] = new int[]{0, 6, 0, 1, 0, 0, 8, 0, 2};
+        puzzle[7] = new int[]{2, 0, 1, 0, 9, 6, 0, 4, 0};
+        puzzle[8] = new int[]{0, 3, 0, 0, 8, 0, 1, 0, 6};
         return puzzle;
-        
     }
     
     private int [][] jawabanPuzzle1(){
         int [][] puzzle = new int [9][9];
-        puzzle[0] = new int[]{5, 2, 3, 1, 9, 4, 6, 8, 7};
-        puzzle[1] = new int[]{6, 8, 7, 3, 4, 2, 1, 5, 9};
-        puzzle[2] = new int[]{1, 9, 4, 6, 7, 5, 3, 2, 8};
-        puzzle[3] = new int[]{4, 6, 2, 5, 1, 9, 8, 3, 7};
-        puzzle[4] = new int[]{3, 1, 8, 4, 2, 7, 5, 9, 6};
-        puzzle[5] = new int[]{2, 7, 9, 8, 6, 3, 4, 5, 1};
-        puzzle[6] = new int[]{9, 5, 6, 7, 3, 1, 7, 4, 2};
-        puzzle[7] = new int[]{7, 4, 5, 2, 8, 6, 9, 1, 3};
-        puzzle[8] = new int[]{8, 3, 1, 9, 5, 2, 6, 7, 4};
+        puzzle[0] = new int[]{9,2,6,4,1,5,3,7,8};
+        puzzle[1] = new int[]{1,5,8,7,6,3,4,2,9};
+        puzzle[2] = new int[]{3,4,7,9,2,8,5,6,1};
+        puzzle[3] = new int[]{5,1,3,6,7,2,9,8,4};
+        puzzle[4] = new int[]{6,9,4,8,3,1,2,5,7};
+        puzzle[5] = new int[]{8,7,2,5,4,9,6,1,3};
+        puzzle[6] = new int[]{4,6,9,1,5,7,8,3,2};
+        puzzle[7] = new int[]{2,8,1,3,9,6,7,4,5};
+        puzzle[8] = new int[]{7,3,5,2,8,4,1,9,6};
         return puzzle;
     }
     
-    private int [][] Puzzle2(){
+    private int[][] Puzzle2() {
         int[][] puzzle = new int[9][9];
-        puzzle[0] = new int[]{5, 2, 3, 0, 0, 0, 6, 8, 7};
-        puzzle[1] = new int[]{0, 0, 0, 0, 4, 0, 0, 0, 9};
-        puzzle[2] = new int[]{1, 0, 0, 0, 0, 0, 3, 0, 0};
-        puzzle[3] = new int[]{4, 0, 0, 5, 0, 0, 8, 0, 0};
-        puzzle[4] = new int[]{0, 0, 0, 0, 2, 0, 0, 0, 0};
-        puzzle[5] = new int[]{0, 0, 9, 0, 6, 0, 0, 0, 1};
-        puzzle[6] = new int[]{0, 0, 0, 7, 0, 0, 0, 0, 2};
-        puzzle[7] = new int[]{7, 4, 0, 0, 8, 0, 0, 0, 0};
-        puzzle[8] = new int[]{8, 3, 1, 0, 0, 0, 6, 7, 0};
+        puzzle[0] = new int[]{7, 0, 0, 5, 0, 4, 0, 6, 0};
+        puzzle[1] = new int[]{0, 4, 9, 0, 6, 0, 1, 0, 5};
+        puzzle[2] = new int[]{6, 0, 0, 8, 0, 9, 0, 7, 0};
+        puzzle[3] = new int[]{3, 0, 2, 0, 5, 0, 4, 0, 6};
+        puzzle[4] = new int[]{0, 1, 0, 4, 0, 2, 0, 5, 0};
+        puzzle[5] = new int[]{4, 0, 5, 0, 9, 0, 2, 0, 8};
+        puzzle[6] = new int[]{0, 9, 0, 2, 0, 6, 0, 0, 1};
+        puzzle[7] = new int[]{5, 0, 7, 0, 3, 0, 6, 4, 0};
+        puzzle[8] = new int[]{0, 6, 0, 9, 0, 5, 0, 2, 0};
+        return puzzle;
+    }
+    
+    private int [][] jawabanPuzzle2(){
+        int [][] puzzle = new int [9][9];
+        puzzle[0] = new int[]{7,3,8,5,1,4,9,6,2};
+        puzzle[1] = new int[]{2,4,9,3,6,7,1,8,5};
+        puzzle[2] = new int[]{6,5,1,8,2,9,3,7,4};
+        puzzle[3] = new int[]{3,8,2,7,5,1,4,9,6};
+        puzzle[4] = new int[]{9,1,6,4,8,2,7,5,3};
+        puzzle[5] = new int[]{4,7,5,6,9,3,2,1,8};
+        puzzle[6] = new int[]{8,9,4,2,7,6,5,3,1};
+        puzzle[7] = new int[]{5,2,7,1,3,8,6,4,9};
+        puzzle[8] = new int[]{1,6,3,9,4,5,8,2,7};
+        return puzzle;
+    }
+    
+    private int[][] Puzzle3() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{6, 0, 5, 0, 3, 0, 4, 0, 9};
+        puzzle[1] = new int[]{0, 3, 0, 5, 0, 8, 0, 6, 0};
+        puzzle[2] = new int[]{2, 0, 9, 0, 6, 0, 5, 0, 8};
+        puzzle[3] = new int[]{8, 0, 0, 3, 0, 9, 0, 2, 0};
+        puzzle[4] = new int[]{0, 2, 6, 0, 7, 0, 3, 4, 0};
+        puzzle[5] = new int[]{0, 7, 0, 2, 0, 6, 0, 0, 1};
+        puzzle[6] = new int[]{1, 0, 7, 0, 8, 0, 0, 5, 0};
+        puzzle[7] = new int[]{0, 4, 0, 9, 0, 5, 6, 0, 7};
+        puzzle[8] = new int[]{5, 0, 8, 0, 2, 0, 9, 0, 0};
+        return puzzle;
+    }
+    
+    private int [][] jawabanPuzzle3(){
+        int [][] puzzle = new int [9][9];
+        puzzle[0] = new int[]{6,8,5,1,3,2,4,7,9};
+        puzzle[1] = new int[]{7,3,4,5,9,8,1,6,2};
+        puzzle[2] = new int[]{2,1,9,7,6,4,5,3,8};
+        puzzle[3] = new int[]{8,5,1,3,4,9,7,2,6};
+        puzzle[4] = new int[]{9,2,6,8,7,1,3,4,5};
+        puzzle[5] = new int[]{4,7,3,2,5,6,8,9,1};
+        puzzle[6] = new int[]{1,9,7,6,8,3,2,5,4};
+        puzzle[7] = new int[]{3,4,2,9,1,5,6,8,7};
+        puzzle[8] = new int[]{5,6,8,4,2,7,9,1,3};
+        return puzzle;
+    }
+    
+    private int[][] Puzzle4() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{3, 0, 1, 0, 7, 0, 9, 0, 4};
+        puzzle[1] = new int[]{0, 8, 0, 5, 0, 9, 0, 1, 0};
+        puzzle[2] = new int[]{4, 0, 9, 0, 3, 0, 2, 0, 7};
+        puzzle[3] = new int[]{9, 0, 6, 0, 5, 0, 8, 0, 2};
+        puzzle[4] = new int[]{0, 2, 0, 3, 0, 6, 0, 4, 0};
+        puzzle[5] = new int[]{5, 0, 3, 0, 8, 0, 1, 0, 6};
+        puzzle[6] = new int[]{0, 3, 0, 9, 0, 8, 0, 6, 0};
+        puzzle[7] = new int[]{8, 0, 5, 0, 6, 0, 4, 2, 0};
+        puzzle[8] = new int[]{0, 9, 0, 4, 0, 5, 0, 0, 8};
+        return puzzle;
+    }
+    
+    private int[][] jawabanPuzzle4() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{3, 5, 1, 6, 7, 2, 9, 8, 4};
+        puzzle[1] = new int[]{2, 8, 7, 5, 4, 9, 6, 1, 3};
+        puzzle[2] = new int[]{4, 6, 9, 8, 3, 1, 2, 5, 7};
+        puzzle[3] = new int[]{9, 4, 6, 1, 5, 7, 8, 3, 2};
+        puzzle[4] = new int[]{1, 2, 8, 3, 9, 6, 7, 4, 5};
+        puzzle[5] = new int[]{5, 7, 3, 2, 8, 4, 1, 9, 6};
+        puzzle[6] = new int[]{7, 3, 4, 9, 2, 8, 5, 6, 1};
+        puzzle[7] = new int[]{8, 1, 5, 7, 6, 3, 4, 2, 9};
+        puzzle[8] = new int[]{6, 9, 2, 4, 1, 5, 3, 7, 8};
+        return puzzle;
+    }
+    
+    private int[][] Puzzle5() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{4, 0, 7, 0, 1, 0, 6, 0, 5};
+        puzzle[1] = new int[]{0, 2, 0, 9, 0, 8, 0, 3, 0};
+        puzzle[2] = new int[]{5, 0, 3, 0, 7, 0, 2, 0, 9};
+        puzzle[3] = new int[]{0, 6, 0, 4, 0, 9, 0, 5, 0};
+        puzzle[4] = new int[]{3, 0, 4, 0, 8, 0, 9, 0, 6};
+        puzzle[5] = new int[]{0, 1, 0, 5, 0, 6, 0, 7, 0};
+        puzzle[6] = new int[]{9, 0, 1, 0, 4, 0, 5, 0, 8};
+        puzzle[7] = new int[]{0, 7, 0, 1, 0, 5, 0, 4, 0};
+        puzzle[8] = new int[]{2, 0, 5, 0, 6, 0, 1, 0, 7};
+        return puzzle;
+    }
+    
+    private int[][] jawabanPuzzle5() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{4, 9, 7, 3, 1, 2, 6, 8, 5};
+        puzzle[1] = new int[]{1, 2, 6, 9, 5, 8, 7, 3, 4};
+        puzzle[2] = new int[]{5, 8, 3, 6, 7, 4, 2, 1, 9};
+        puzzle[3] = new int[]{7, 6, 2, 4, 3, 9, 8, 5, 1};
+        puzzle[4] = new int[]{3, 5, 4, 7, 8, 1, 9, 2, 6};
+        puzzle[5] = new int[]{8, 1, 9, 5, 2, 6, 4, 7, 3};
+        puzzle[6] = new int[]{9, 3, 1, 2, 4, 7, 5, 6, 8};
+        puzzle[7] = new int[]{6, 7, 8, 1, 9, 5, 3, 4, 2};
+        puzzle[8] = new int[]{2, 4, 5, 8, 6, 3, 1, 9, 7};
+        return puzzle;
+    }
+    
+    private int[][] Puzzle6() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{6, 0, 2, 0, 1, 0, 8, 0, 9};
+        puzzle[1] = new int[]{0, 3, 0, 6, 0, 4, 0, 7, 0};
+        puzzle[2] = new int[]{5, 0, 9, 0, 7, 0, 1, 0, 6};
+        puzzle[3] = new int[]{0, 9, 0, 2, 0, 1, 0, 5, 0};
+        puzzle[4] = new int[]{1, 0, 7, 0, 6, 0, 3, 0, 8};
+        puzzle[5] = new int[]{0, 8, 0, 7, 0, 5, 0, 6, 0};
+        puzzle[6] = new int[]{4, 0, 5, 0, 2, 0, 7, 0, 3};
+        puzzle[7] = new int[]{0, 6, 0, 1, 0, 8, 0, 9, 0};
+        puzzle[8] = new int[]{9, 0, 8, 0, 4, 0, 6, 0, 5};
+        return puzzle;
+    }
+    
+    private int[][] jawabanPuzzle6() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{6, 7, 2, 5, 1, 3, 8, 4, 9};
+        puzzle[1] = new int[]{8, 3, 1, 6, 9, 4, 5, 7, 2};
+        puzzle[2] = new int[]{5, 4, 9, 8, 7, 2, 1, 3, 6};
+        puzzle[3] = new int[]{3, 9, 6, 2, 8, 1, 4, 5, 7};
+        puzzle[4] = new int[]{1, 5, 7, 4, 6, 9, 3, 2, 8};
+        puzzle[5] = new int[]{2, 8, 4, 7, 3, 5, 9, 6, 1};
+        puzzle[6] = new int[]{4, 1, 5, 9, 2, 6, 7, 8, 3};
+        puzzle[7] = new int[]{7, 6, 3, 1, 5, 8, 2, 9, 4};
+        puzzle[8] = new int[]{9, 2, 8, 3, 4, 7, 6, 1, 5};
+        return puzzle;
+    }
+    
+    private int[][] Puzzle7() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{3, 0, 5, 0, 1, 0, 6, 0, 9};
+        puzzle[1] = new int[]{0, 2, 0, 3, 0, 4, 0, 5, 0};
+        puzzle[2] = new int[]{8, 0, 1, 0, 6, 0, 3, 0, 4};
+        puzzle[3] = new int[]{0, 7, 0, 1, 0, 3, 0, 8, 0};
+        puzzle[4] = new int[]{1, 0, 2, 0, 8, 0, 4, 0, 7};
+        puzzle[5] = new int[]{0, 3, 0, 7, 0, 6, 0, 1, 0};
+        puzzle[6] = new int[]{9, 0, 3, 0, 7, 0, 8, 0, 5};
+        puzzle[7] = new int[]{0, 8, 0, 9, 0, 1, 0, 4, 0};
+        puzzle[8] = new int[]{2, 0, 4, 0, 3, 0, 7, 0, 1};
+        return puzzle;
+    }
+    
+    
+    private int[][] jawabanPuzzle7() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{3, 4, 5, 8, 1, 7, 6, 2, 9};
+        puzzle[1] = new int[]{7, 2, 6, 3, 9, 4, 1, 5, 8};
+        puzzle[2] = new int[]{8, 9, 1, 2, 6, 5, 3, 7, 4};
+        puzzle[3] = new int[]{4, 7, 9, 1, 2, 3, 5, 8, 6};
+        puzzle[4] = new int[]{1, 6, 2, 5, 8, 9, 4, 3, 7};
+        puzzle[5] = new int[]{5, 3, 8, 7, 4, 6, 9, 1, 2};
+        puzzle[6] = new int[]{9, 1, 3, 4, 7, 2, 8, 6, 5};
+        puzzle[7] = new int[]{6, 8, 7, 9, 5, 1, 2, 4, 3};
+        puzzle[8] = new int[]{2, 5, 4, 6, 3, 8, 7, 9, 1};
+        return puzzle;
+    }
+    
+    private int[][] Puzzle8() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{9, 0, 4, 0, 1, 0, 7, 0, 6};
+        puzzle[1] = new int[]{0, 1, 0, 2, 0, 8, 0, 9, 0};
+        puzzle[2] = new int[]{2, 0, 7, 0, 9, 0, 3, 0, 8};
+        puzzle[3] = new int[]{0, 3, 0, 9, 0, 4, 0, 7, 0};
+        puzzle[4] = new int[]{7, 0, 5, 0, 8, 0, 9, 0, 3};
+        puzzle[5] = new int[]{0, 9, 0, 5, 0, 7, 0, 4, 0};
+        puzzle[6] = new int[]{3, 0, 8, 0, 2, 0, 1, 0, 4};
+        puzzle[7] = new int[]{0, 2, 0, 8, 0, 1, 0, 3, 0};
+        puzzle[8] = new int[]{5, 0, 1, 0, 4, 0, 2, 0, 9};
+        return puzzle;
+    }
+ 
+    private int[][] jawabanPuzzle8() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{9, 8, 4, 3, 1, 5, 7, 2, 6};
+        puzzle[1] = new int[]{6, 1, 3, 2, 7, 8, 4, 9, 5};
+        puzzle[2] = new int[]{2, 5, 7, 4, 9, 6, 3, 1, 8};
+        puzzle[3] = new int[]{8, 3, 2, 9, 6, 4, 5, 7, 1};
+        puzzle[4] = new int[]{7, 4, 5, 1, 8, 2, 9, 6, 3};
+        puzzle[5] = new int[]{1, 9, 6, 5, 3, 7, 8, 4, 2};
+        puzzle[6] = new int[]{3, 7, 8, 6, 2, 9, 1, 5, 4};
+        puzzle[7] = new int[]{4, 2, 9, 8, 5, 1, 6, 3, 7};
+        puzzle[8] = new int[]{5, 6, 1, 7, 4, 3, 2, 8, 9};
+        return puzzle;
+    }
+    
+    private int[][] Puzzle9() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{2, 0, 9, 0, 1, 0, 7, 0, 8};
+        puzzle[1] = new int[]{0, 8, 0, 7, 0, 3, 0, 4, 0};
+        puzzle[2] = new int[]{4, 0, 3, 0, 2, 0, 6, 0, 1};
+        puzzle[3] = new int[]{0, 2, 0, 5, 0, 9, 0, 6, 0};
+        puzzle[4] = new int[]{9, 0, 6, 0, 3, 0, 5, 0, 7};
+        puzzle[5] = new int[]{0, 3, 0, 6, 0, 2, 0, 9, 0};
+        puzzle[6] = new int[]{6, 0, 4, 0, 5, 0, 3, 0, 2};
+        puzzle[7] = new int[]{0, 1, 0, 3, 0, 6, 0, 7, 0};
+        puzzle[8] = new int[]{3, 0, 7, 0, 8, 0, 9, 0, 6};
+        return puzzle;
+    }
+    
+    private int[][] jawabanPuzzle9() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{2, 6, 9, 4, 1, 5, 7, 3, 8};
+        puzzle[1] = new int[]{5, 8, 1, 7, 6, 3, 2, 4, 9};
+        puzzle[2] = new int[]{4, 7, 3, 9, 2, 8, 6, 5, 1};
+        puzzle[3] = new int[]{7, 2, 8, 5, 4, 9, 1, 6, 3};
+        puzzle[4] = new int[]{9, 4, 6, 8, 3, 1, 5, 2, 7};
+        puzzle[5] = new int[]{1, 3, 5, 6, 7, 2, 8, 9, 4};
+        puzzle[6] = new int[]{6, 9, 4, 1, 5, 7, 3, 8, 2};
+        puzzle[7] = new int[]{8, 1, 2, 3, 9, 6, 4, 7, 5};
+        puzzle[8] = new int[]{3, 5, 7, 2, 8, 4, 9, 1, 6};
+        return puzzle;
+    }
+    
+    private int[][] Puzzle10() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{9, 0, 8, 0, 6, 0, 3, 0, 7};
+        puzzle[1] = new int[]{0, 6, 0, 4, 0, 9, 0, 5, 0};
+        puzzle[2] = new int[]{4, 0, 5, 0, 7, 0, 9, 0, 6};
+        puzzle[3] = new int[]{0, 5, 0, 8, 0, 2, 0, 6, 0};
+        puzzle[4] = new int[]{3, 0, 6, 0, 4, 0, 2, 0, 1};
+        puzzle[5] = new int[]{0, 8, 0, 1, 0, 6, 0, 3, 0};
+        puzzle[6] = new int[]{6, 0, 2, 0, 8, 0, 5, 0, 3};
+        puzzle[7] = new int[]{0, 3, 0, 2, 0, 7, 0, 9, 0};
+        puzzle[8] = new int[]{5, 0, 9, 0, 1, 0, 8, 0, 2};
+        return puzzle;
+    }
+    
+    
+    private int[][] jawabanPuzzle10() {
+        int[][] puzzle = new int[9][9];
+        puzzle[0] = new int[]{9, 2, 8, 5, 6, 1, 3, 4, 7};
+        puzzle[1] = new int[]{7, 6, 3, 4, 2, 9, 1, 5, 8};
+        puzzle[2] = new int[]{4, 1, 5, 3, 7, 8, 9, 2, 6};
+        puzzle[3] = new int[]{1, 5, 7, 8, 3, 2, 4, 6, 9};
+        puzzle[4] = new int[]{3, 9, 6, 7, 4, 5, 2, 8, 1};
+        puzzle[5] = new int[]{2, 8, 4, 1, 9, 6, 7, 3, 5};
+        puzzle[6] = new int[]{6, 7, 2, 9, 8, 4, 5, 1, 3};
+        puzzle[7] = new int[]{8, 3, 1, 2, 5, 7, 6, 9, 4};
+        puzzle[8] = new int[]{5, 4, 9, 6, 1, 3, 8, 7, 2};
         return puzzle;
     }
 
@@ -273,31 +513,43 @@ public class Logic extends JFrame implements Runnable{
         sudokuPanel.setBackground(Color.WHITE);
         sudokuPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         int random = rand.nextInt(10+1);
+        int[][] puzzleTemp = null;
+        int[][] jawabanTemp = null;
         
-//        if(random ==1){
-//            int[][] puzzle = Puzzle1();
-//        }else if(random ==2){
-//            int[][] puzzle = Puzzle2();
-//        }else if(random ==3){
-//            int[][] puzzle = Puzzle3();
-//        }else if(random ==4){
-//            int[][] puzzle = Puzzle4();
-//        }else if(random ==5){
-//            int[][] puzzle = Puzzle5();
-//        }else if(random ==6){
-//            int[][] puzzle = Puzzle6();
-//        }else if(random ==7){
-//            int[][] puzzle = Puzzle7();
-//        }else if(random ==8){
-//            int[][] puzzle = Puzzle8();
-//        }else if(random ==9){
-//            int[][] puzzle = Puzzle9();
-//        }else if(random ==10){
-//            int[][] puzzle = Puzzle10();
-//        }
-
-        int[][] puzzle = Puzzle1();
-        int[][] jawabanPuzzle = jawabanPuzzle1();
+        if(random ==1){
+            puzzleTemp = Puzzle1();
+            jawabanTemp = jawabanPuzzle1();
+        }else if(random ==2){
+            puzzleTemp = Puzzle2();
+            jawabanTemp = jawabanPuzzle2();
+        }else if(random ==3){
+            puzzleTemp = Puzzle3();
+            jawabanTemp = jawabanPuzzle3();
+        }else if(random ==4){
+            puzzleTemp = Puzzle4();
+            jawabanTemp = jawabanPuzzle4();
+        }else if(random ==5){
+            puzzleTemp = Puzzle5();
+            jawabanTemp = jawabanPuzzle5();
+        }else if(random ==6){
+            puzzleTemp = Puzzle6();
+            jawabanTemp = jawabanPuzzle6();
+        }else if(random ==7){
+            puzzleTemp = Puzzle7();
+            jawabanTemp = jawabanPuzzle7();
+        }else if(random ==8){
+            puzzleTemp = Puzzle8();
+            jawabanTemp = jawabanPuzzle8();
+        }else if(random ==9){
+            puzzleTemp = Puzzle9();
+            jawabanTemp = jawabanPuzzle9();
+        }else if(random ==10){
+            puzzleTemp = Puzzle10();
+            jawabanTemp = jawabanPuzzle10();
+        }
+        
+        final int[][] puzzle = puzzleTemp;
+        final int[][] jawabanPuzzle = jawabanTemp;
         
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
@@ -387,7 +639,18 @@ public class Logic extends JFrame implements Runnable{
                     Logic game = new Logic();
                     game.game();
                     game.setVisible(true);
-                }else{}
+                }else{
+                    JOptionPane.showMessageDialog(
+                        this,
+                        "Kredit tidak cukup untuk bermain!\n",
+                        "",
+                        JOptionPane.WARNING_MESSAGE
+                    );
+                    dispose();
+                    Logic menu = new Logic();
+                    menu.menuGame();
+                    menu.setVisible(true);
+                }
             });
             kotak.add(playAgain);
             
@@ -454,7 +717,19 @@ public class Logic extends JFrame implements Runnable{
                     Logic game = new Logic();
                     game.game();
                     game.setVisible(true);
-                }else{}
+                }else{
+                    JOptionPane.showMessageDialog(
+                        this,
+                        "Kredit tidak cukup untuk bermain!\n",
+                        "",
+                        JOptionPane.WARNING_MESSAGE
+                    );
+                    dispose();
+                    Logic menu = new Logic();
+                    menu.menuGame();
+                    menu.setVisible(true);
+                    
+                }
             });
             kotak.add(playAgain);
             
@@ -583,7 +858,12 @@ public class Logic extends JFrame implements Runnable{
                     game.setVisible(true);
                 });
             }else{
-                
+                JOptionPane.showMessageDialog(
+                    this,
+                    "Kredit tidak cukup untuk bermain!\n",
+                    "",
+                    JOptionPane.WARNING_MESSAGE
+                );
             }
         });
         button.add(btnStart,BorderLayout.NORTH);
